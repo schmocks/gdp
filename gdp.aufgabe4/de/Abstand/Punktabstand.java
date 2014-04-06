ba868ed26066383dd5ca291f0e1a0ee0d71a0ba4
Stephan <Stephan@PC.fritz.box>
2014-04-06 17:39:34 +0200

    MI

--- gdp.aufgabe4/de/Abstand/Punktabstand.java
+++ gdp.aufgabe4/de/Abstand/Punktabstand.java
@@ -6,11 +6,15 @@ import java.util.Scanner;
 public class Punktabstand {
 	static ArrayList<Punkt> Punkte = new ArrayList<Punkt>();
 	
+	
 	public static void main(String[] args) {
-		
-	PunktEingabe();
-	PunkteZeichnen();
-	PunktDistanz();
+	
+	
+	punkteEingabe();
+	punktePruefen();
+	punkteZeichnen(punkteSortieren());
+	punkteDistanz();
+	
 		
 	}
 	
@@ -19,19 +23,40 @@ public class Punktabstand {
 	 * Punkt eingeben
 	 */
 
-	static void PunktEingabe(){
+	static void punkteEingabe(){
+		boolean b1=true;
 		int n = 0;
+		int x =0;
+		int y =0;
 		Scanner input = new Scanner(System.in);
-		System.out.print("Bitte Anzahl der Punkte eingeben: ");
-		if(input.hasNextInt()){n=input.nextInt();}
-		int[] x = new int[n];
-		int[] y = new int[n];
+		do{
+			System.out.print("Bitte Anzahl der Punkte eingeben: ");
+			if(input.hasNextInt()){
+				b1=false;
+				n=input.nextInt();
+			}
+			input.nextLine();			
+		}while(b1);
+		
 		for(int i=1;i<=n;i++){
-			System.out.print("X-Wert des "+i+". Punktes: ");
-			if(input.hasNextInt()){x[i-1]=input.nextInt();}
-			System.out.print("Y-Wert des "+i+". Punktes: ");
-			if(input.hasNextInt()){y[i-1]=input.nextInt();}
-			Punkte.add(new Punkt(i, x[i-1], y[i-1]));
+			boolean b2=true,b3=true;
+			do{
+				System.out.print("X-Wert des "+i+". Punktes: ");
+				if(input.hasNextInt()){
+					b2=false;
+					x=input.nextInt();
+				}
+				input.nextLine();
+			}while(b2);
+			do{
+				System.out.print("Y-Wert des "+i+". Punktes: ");
+				if(input.hasNextInt()){
+					b3=false;
+					y=input.nextInt();
+				}
+				input.nextLine();
+			}while(b3);
+			Punkte.add(new Punkt(i, x, y));
 			}
 		input.close();
 	}
@@ -40,11 +65,11 @@ public class Punktabstand {
 	 * Zeichnet Punkte
 	 */
 	
-	static void PunkteZeichnen(){
+	static void punkteZeichnen(Punkt[] punkte){
 		int xmax=0,ymax=0;
-		for(int i=0;i<Punkte.size();i++){
-			if(Punkte.get(i).getX()>xmax){xmax=Punkte.get(i).getX();}
-			if(Punkte.get(i).getY()>ymax){ymax=Punkte.get(i).getY();}
+		for(int i=0;i<punkte.length;i++){
+			if(punkte[i].getX()>xmax){xmax=punkte[i].getX();}
+			if(punkte[i].getY()>ymax){ymax=punkte[i].getY();}
 			}
 		
 		System.out.print("\n [y]");
@@ -52,7 +77,7 @@ public class Punktabstand {
 			if(i>99){System.out.print("\n"+i);}
 			if(i>9&&i<100){System.out.print("\n "+i);}
 			if(i<10&&i>0){System.out.print("\n  "+i);}
-			zeichneZeile(i);
+			zeichneZeile(punkte, i);
 			if(i==0){
 				System.out.print("\n  0  ");
 				for(int j=1; j<=xmax;j++){
@@ -70,18 +95,24 @@ public class Punktabstand {
 	 * @param Zeilennummer
 	 * @return Zeile als String
 	 */
-	static String zeichneZeile(int i){
-		
-		for(int x=0;x<Punkte.size();x++){
-			if(Punkte.get(x).getY()==i){
+	static String zeichneZeile(Punkt[] punkte,int i){
+		String space = "  ",sp1 = " ", sp2 = "  ",sp3 = "   ";
+		int s=0,anz=0;
+		for(int x=0;x<punkte.length;x++){
+			if(punkte[x].getY()==i){
 				System.out.print("");
-				for(int y=0;y<Punkte.get(x).getX();y++){
-					if(y==0){System.out.print("  ");}
-					if(y>0&&y<10){System.out.print("   ");}
-					if(y>=10&&y<100){System.out.print("    ");}
-					if(y>=100){System.out.print("    ");}
+				for(int y=s;y<punkte[x].getX();y++){
+					
+					if(y>0&&y<10){space=space+sp1+sp2;}
+					if(y>=10&&y<100){space=space+sp2+sp2;}
+					if(y>=100){space=space+sp2+sp3;}
+					
 				}
-				System.out.print("x" + Punkte.get(x).getIndex());
+				if(anz!=0){space=space.substring(anz-1);}
+				System.out.print(space+"x" + punkte[x].getIndex());
+				s=punkte[x].getX()+1;
+				//space=space.substring(1);
+				anz+=2;
 			}
 		}
 		return null;
@@ -90,18 +121,57 @@ public class Punktabstand {
 	/**
 	 * errechnet Punktdistanz
 	 */	
-	static void PunktDistanz(){
+	static void punkteDistanz(){
 		for(int i=1;i<Punkte.size();i++){
-			System.out.print("Entfernung P" + Punkte.get(0).getIndex() + " > P" + Punkte.get(i).getIndex() + ": ");
+			System.out.print("Entfernung P" + Punkte.get(0).getIndex() + " <-> P" + Punkte.get(i).getIndex() + ": ");
 			System.out.println(Punkte.get(0).getAbstand(Punkte.get(i)));
 		}
 		Punkte.remove(0);
-		if(Punkte.size()>1){PunktDistanz();}
+		if(Punkte.size()>1){punkteDistanz();}
 		
 	}
 	
+	static Punkt[] punkteSortieren(){
+		Punkt[] punkte = Punkte.toArray(new Punkt[Punkte.size()]);
+		Punkt temp;
+		for(int i=1; i<punkte.length; i++) {
+			for(int j=0; j<punkte.length-i; j++) {
+				if(punkte[j].getX()>punkte[j+1].getX()) {
+					temp=punkte[j]; punkte[j]=punkte[j+1]; punkte[j+1]=temp;
+					}
+				}
+			}
+		return punkte;
 		
+					
+	}
 		
+	static void punktePruefen(){
+		System.out.println("\nEingegebene Punkte: " + Punkte);
+		boolean is = false;
+		Punkt[] punkte = Punkte.toArray(new Punkt[Punkte.size()]);
+		ArrayList<Punkt> rem = new ArrayList<Punkt>();
+		rem=Punkte;
+		for(int i = 0; i<punkte.length;i++){
+			for(int j = i+1;j<punkte.length;j++){
+				if(punkte[i].getX()==punkte[j].getX()&&punkte[i].getY()==punkte[j].getY()){
+					int p1 = punkte[i].getIndex();
+					int p2 = punkte[j].getIndex();
+					System.out.println("P"+ p1 +" und P"+p2+" sind identisch, P"+p2+" wird entfernt!");
+					is = true;
+					if(rem.contains(punkte[j])){
+						rem.remove(punkte[j]);						
+					}
+				}
+				
+			}
+		}
+		Punkte=rem;
+		if(is){
+			System.out.println("Folgende Punkte werden gezeichnet: " + Punkte);
+		}
+			
+	}
 		
 		
 	
